dfe97ac5a99aafda25276b242f91867d9e5398c5
henryschwerdtner <henry.schwerdtner@web.de>
2024-09-16 09:37:55 +0200

    make index non blocking

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@ const {getProducts} = require("./scrape");
 const {updateDiscountsOnServer, checkIfRepoIsEmpty} = require("./util");
 
 
-async function main() {
-    const isRepoEmpty = await checkIfRepoIsEmpty();
-    if (isRepoEmpty) {
+checkIfRepoIsEmpty().then((isEmpty) => {
+    console.log("Repo is empty: ", isEmpty);
+    if (isEmpty) {
         console.log("Start scrape on server start");
-        const products = await getProducts();
-        await updateDiscountsOnServer(products);
+        getProducts().then((products) => {
+            updateDiscountsOnServer(products).then(() => {
+                console.log("Scrape finished");
+            });
+        });
     }
-    nodeSchedule.scheduleJob("* 30 8 * * 1", async () => {
-        console.log("Start scrape on Monday at 8:00 AM");
-        const products = await getProducts();
-        await updateDiscountsOnServer(products);
-    });
-}
-main();
+})
+
+nodeSchedule.scheduleJob("* 30 8 * * 1", async () => {
+    console.log("Start scrape on Monday at 8:00 AM");
+    const products = await getProducts();
+    await updateDiscountsOnServer(products);
+});
